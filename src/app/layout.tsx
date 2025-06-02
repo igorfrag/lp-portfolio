4131f1c5c5c7e713c871475b6845383d2d0e1a98
igorfrag <igorfragoso3@gmail.com>
2025-06-02 20:23:01 -0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,16 @@ export default function RootLayout({
 }) {
     return (
         <html lang='pt-BR'>
+            <head>
+                <meta charSet='utf-8' />
+                <meta name='author' content='Igor Fragoso' />
+                <meta
+                    name='description'
+                    content='Portfolio do desenvolvedor Igor Fragoso'
+                />
+                <link rel='icon' href='/assets/favicon.ico' />
+                <title>Igor Fragoso - Developer</title>
+            </head>
             <body className='flex flex-col  text-white bg-gray-900  '>
                 <Header />
                 <main className='mb-auto h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth overflow-x-hidden'>
